cffbb7eafa1b3d0b87b9611c1a07d2abaf48aa3c
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-08-09 14:35:32 -0400

    Fix bugs

--- js/ main.js
+++
@@ -1,6 +0,0 @@
-var hiddenMessage = '<p><span>☆</span>Superstar!</p>;
-var i = 0;
-
-for (i = 0; i < 10 i++) {
-  document.write(hiddenMessage);
-
---
+++ js/main.js
@@ -0,0 +1,6 @@
+var hiddenMessage = '<p><span>☆</span>Superstar!</p>';
+var i = 0;
+
+for (i = 0; i < 10; i++) {
+  document.write(hiddenMessage);
+};
